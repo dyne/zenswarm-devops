bdcc9d44307be30c3b691f27c1ad42c65396e0f5
Jaromil <jaromil@dyne.org>
2022-02-28 07:22:34 +0100

    packer builder for linode golden image based on devuan

---
+++ packer/install_python.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# DEBIAN_FRONTEND=noninteractive apt-get update -q -y
+#DEBIAN_FRONTEND=noninteractive apt-get install -q -y python ansible
+DEBIAN_FRONTEND=noninteractive apt-get install -q -y --no-install-recommends python3 curl
