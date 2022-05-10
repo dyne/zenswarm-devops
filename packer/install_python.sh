6c544cfc60c0dbac6cfba4ef7a62d8264133bd27
Jaromil <jaromil@dyne.org>
2022-05-10 15:58:09 +0200

    clean old ansibles

--- packer/install_python.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# DEBIAN_FRONTEND=noninteractive apt-get update -q -y
-#DEBIAN_FRONTEND=noninteractive apt-get install -q -y python ansible
-DEBIAN_FRONTEND=noninteractive apt-get install -q -y --no-install-recommends python3 curl
