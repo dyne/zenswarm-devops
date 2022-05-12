8f72a097aefeffe1c8fd223f71272e581050d16d
Alberto Lerda <albertolerda97@gmail.com>
2022-05-12 08:30:09 +0200

    feat: include manual deployment
    fix: improved ansible

---
+++ roles/manual-deployment/instance-provisioning.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# TWEAK SETTINGS
+NODES=2
+REPO=https://github.com/dyne/restroom-vmlet-template
+CONTRACTS=saved_files.sh
+
+# UTILITIES
+BASEPATH=$(pwd)
+# HOST=$(hostname -I)
+# changed to (hostname) cause the current staging uses a shared ip
+HOST=$(hostname -I | awk '{print $1;}')
+
+# DEPENDENCIES
+# command -v python || (sudo apt install -y python)
+# command -v git || (sudo apt install -y git)
+# command -v yarn || (npm install --global yarn)
+# command -v npx || (curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && sudo apt-get install -y nodejs)
+# command -v make || (sudo apt install -y build-essential)
+# command -v pm2 || (yarn global add pm2)
+# dpkg -l libczmq-dev &> /dev/null || (sudo apt install -y libczmq-dev)
+npm install -g node-gyp-cache
+npm config set node_gyp node-gyp-cache
+
+link_build() {
+  if ! test -d $(basename $1); then
+    git clone --recursive https://github.com/$1 && cd $(basename $1)
+    yarn install
+    yarn link
+    cd ..
+  fi
+}
+
+# link_build bitauth/libauth
+# link_build cryptocoinjs/secp256k1-node
+# link_build protobufjs/protobuf.js
+# link_build mapbox/node-sqlite3
+# link_build PeculiarVentures/pkcs11js
+yarn
+
+parallel_job() {
+  local i=$1
+  NODE=restroom-mw-$i
+  WD=$BASEPATH/vmlets/$NODE
+
+  if ! test -d $WD; then
+    npx degit $REPO $WD
+    cd $WD
+    # yarn link @bitauth/libauth
+    # yarn link sqlite3
+    # yarn link protobufjs
+    # yarn link pkcs11js
+    # yarn link secp256k1
+    # yarn add zeromq@5 --zmq-external
+    sed -ie "s/restroom-vmlet-template/${NODE}/g" package.json
+    yarn
+
+  fi
+  cd $WD
+  if ! test -d contracts; then
+    mkdir contracts
+  fi
+  bash $BASEPATH/$CONTRACTS
+  echo "CUSTOM_404_MESSAGE=nothing to see here
+  ZENCODE_DIR="$(pwd)/contracts"
+  FILES_DIR=$(pwd)
+  CHAIN_EXT=chain
+  # OPENAPI=true
+  HOST=$HOST
+  PRIVATE_ZENCODE_DIR="$BASEPATH/private-contracts"
+  YML_EXT=yml"> .env
+
+  pm2 delete $NODE || true
+  pm2 start yarn --update-env --name $NODE --time -- start
+}
+
+pm2 kill
+for i in $(seq $NODES); do
+  parallel_job $i
+done
---
+++ roles/manual-deployment/saved_files.sh
@@ -0,0 +1,116 @@
+echo "
+Rule caller restroom-mw
+
+Given I have a 'string' named 'add-identity'
+Given I have a 'string dictionary' named 'post'
+# Given I have a 'string dictionary' named 'output'
+Given I have a 'string dictionary' named 'output'
+Given I connect to 'add-identity' and pass it the content of 'post' and save the output into 'output'
+
+
+Then print the 'output'
+
+
+
+"> ./contracts/consensusroom-announce.zen
+echo ✔ Imported consensusroom-announce.zen
+
+echo "{}" > ./contracts/consensusroom-announce.keys
+echo ✔ Imported consensusroom-announce.keys
+
+
+
+
+
+echo "
+
+Scenario 'ecdh': Create the keypair
+Scenario 'ethereum': Create the keypair
+Scenario 'reflow': Create the keypair
+Scenario 'schnorr': Create the keypair
+
+Given I have the 'keyring'
+When I create the ecdh public key
+When I create the reflow public key
+When I create the schnorr public key
+
+When I create the bitcoin address
+When I create the ethereum address
+
+Then print the 'ecdh public key'
+Then print the 'reflow public key'
+Then print the 'schnorr public key'
+
+Then print the 'bitcoin address'
+Then print the 'ethereum address'
+
+"> ./contracts/consensusroom-generate-all-public-keys.zen
+echo ✔ Imported consensusroom-generate-all-public-keys.zen
+
+echo "
+Rule caller restroom-mw
+Scenario 'ecdh': verifies the signature from Apiroom 
+Given I have a 'string dictionary' named 'updateInfo'
+Given I have a 'string dictionary' named 'update' inside 'updateInfo'
+Given I have a 'base64 dictionary' named 'update.signature' inside 'updateInfo'
+# Given I have a 'string' named 'myFile' inside 'update'
+Given I have a 'string' named 'myFolder' 
+
+# Given I have a 'signature' named 'update.signature' 
+Given I have a 'ecdh public key' from 'Apiroom' 
+
+When I verify the 'update' has a signature in 'update.signature' by 'Apiroom' 
+
+When I create the copy of 'myFile' from dictionary 'update'
+When I rename the 'copy' to 'myFile'
+
+Then print the string 'Zenroom verified the Apiroom signature are all correct!' 
+Then print the 'myFile'
+Then print the 'myFolder'
+
+Then I download the 'myFile' and extract it into 'myFolder'
+"> ./contracts/consensusroom-update.zen
+echo ✔ Imported consensusroom-update.zen
+
+echo "{\"Apiroom\":{\"ecdh_public_key\":\"BPMbqmvEwUJsB6MmrswxKxza5+Lt82X20mAjcmknT7E7RysW6fACs/L3sXEOEh8qYxHKNAvcBCAetcIEuIEUVC4=\"},\"myFolder\":\"./contracts\"}" > ./contracts/consensusroom-update.keys
+echo ✔ Imported consensusroom-update.keys
+
+echo "
+Rule caller restroom-mw
+Given I have a 'string' named 'myTimestamp'
+Given I fetch the local timestamp and store it into 'myTimestamp'
+Then print 'myTimestamp'
+"> ./contracts/consensusroom-get-timestamp.zen
+echo ✔ Imported consensusroom-get-timestamp.zen
+
+
+echo "
+Rule unknown ignore
+Given I connect to 'endpoint' and pass it the content of 'myDataToPOST' and save the output into 'dataFromEndpoint'
+Given I have a 'string dictionary' named 'dataFromEndpoint'
+Then print the 'dataFromEndpoint'
+"> ./contracts/consensusroom-http-post.zen
+echo ✔ Imported consensusroom-http-post.zen
+
+echo "
+Given nothing
+Then print the string 'I am alive!'
+"> ./contracts/consensusroom-ping.zen
+echo ✔ Imported consensusroom-ping.zen
+
+echo "
+ledgers:
+  ping:
+    interval: 300
+    contract: consensusroom-ping
+"> ./L1.yaml
+echo ✔ Imported L1.yaml
+
+
+echo "
+Rule caller restroom-mw
+Given I connect to 'endpoint' and pass it the content of 'post' and save the output into 'output'
+Given I have a 'string dictionary' named 'output'
+Then print the 'output'
+"> ./contracts/consensusroom-http-post.zen
+echo ✔ Imported onsensusroom-http-post.zen
